c236eacf6a46de14cabb73cc01d17b8123c421e9
Vinay Jain <vinayjain7281@gmail.com>
2024-07-02 09:37:25 +0530

    First commit

---
+++ backend/tunein/src/main/java/org/teadev/tunein/TuneinApplication.java
@@ -0,0 +1,13 @@
+package org.teadev.tunein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneinApplication.class, args);
+	}
+
+}
---
+++ backend/tunein/src/test/java/org/teadev/tunein/TuneinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teadev.tunein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
