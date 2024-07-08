287601e6bad0fe86c8a80649c682345b06d0f394
Vinay Jain <vinayjain7281@gmail.com>
2024-07-08 10:21:02 +0530

    - Add missing files for storage server.

---
+++ tunein_storage/src/main/java/org/teadev/tunein_storage/TuneinStorageApplication.java
@@ -0,0 +1,13 @@
+package org.teadev.tunein_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneinStorageApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TuneinStorageApplication.class, args);
+    }
+    
+}
