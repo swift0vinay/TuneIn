dc6cf4694fa9afd7ca65481c53a60e92af38af18
Vinay Jain <vinayjain7281@gmail.com>
2024-07-08 10:23:15 +0530

    - Add missing files for storage server.

---
+++ tunein_storage/src/test/java/org/teadev/tunein_storage/TuneinStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teadev.tunein_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneinStorageApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
